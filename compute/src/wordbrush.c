f1240d0e67793e08f2ee670bde9baacbda2b7316
Tom Harley <tom@magnostherobot.co.uk>
2020-09-22 20:08:15 +0100

    Prettied-up key position definitions
    Used a macro to greatly reduce the code size of the
    definitions of positions of keys on a keyboard.

--- compute/src/wordbrush.c
+++ compute/src/wordbrush.c
@@ -2,94 +2,54 @@
 
 #include "wordbrush.h"
 
+#define key(c, wo, ho) \
+	case c: \
+    	    return (KeyOffset) { \
+    	        .width_offset = wo, \
+	        .width_percentage = KEY_WIDTH_PERCENTAGE, \
+	        .height_offset = ho, \
+    	        .height_percentage = KEY_HEIGHT_PERCENTAGE \
+    	    }
+
 KeyOffset getCharacterOffset(char character) {
-    KeyOffset ret;
     switch (character) {
-        case 'q':
-            ret = (KeyOffset) {.width_offset=0, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=0, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'w':
-            ret = (KeyOffset) {.width_offset=1, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=0, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'e':
-            ret = (KeyOffset) {.width_offset=2, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=0, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'r':
-            ret = (KeyOffset) {.width_offset=3, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=0, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 't':
-            ret = (KeyOffset) {.width_offset=4, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=0, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'y':
-            ret = (KeyOffset) {.width_offset=5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=0, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'u':
-            ret = (KeyOffset) {.width_offset=6, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=0, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'i':
-            ret = (KeyOffset) {.width_offset=7, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=0, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'o':
-            ret = (KeyOffset) {.width_offset=8, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=0, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'p':
-            ret = (KeyOffset) {.width_offset=9, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=0, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'a':
-            ret = (KeyOffset) {.width_offset=0.5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=1, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 's':
-            ret = (KeyOffset) {.width_offset=1.5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=1, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'd':
-            ret = (KeyOffset) {.width_offset=2.5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=1, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'f':
-            ret = (KeyOffset) {.width_offset=3.5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=1, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'g':
-            ret = (KeyOffset) {.width_offset=4.5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=1, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'h':
-            ret = (KeyOffset) {.width_offset=5.5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=1, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'j':
-            ret = (KeyOffset) {.width_offset=6.5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=1, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'k':
-            ret = (KeyOffset) {.width_offset=7.5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=1, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'l':
-            ret = (KeyOffset) {.width_offset=8.5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=1, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'z':
-            ret = (KeyOffset) {.width_offset=1.5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=2, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'x':
-            ret = (KeyOffset) {.width_offset=2.5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=2, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'c':
-            ret = (KeyOffset) {.width_offset=3.5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=2, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'v':
-            ret = (KeyOffset) {.width_offset=4.5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=2, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'b':
-            ret = (KeyOffset) {.width_offset=5.5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=2, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'n':
-            ret = (KeyOffset) {.width_offset=6.5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=2, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
-        case 'm':
-            ret = (KeyOffset) {.width_offset=7.5, .width_percentage=KEY_WIDTH_PERCENTAGE, .height_offset=2, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
+        key('q', 0, 0);
+        key('w', 1, 0);
+        key('e', 2, 0);
+        key('r', 3, 0);
+        key('t', 4, 0);
+        key('y', 5, 0);
+        key('u', 6, 0);
+        key('i', 7, 0);
+        key('o', 8, 0);
+        key('p', 9, 0);
+        key('a', 0.5, 1);
+        key('s', 1.5, 1);
+        key('d', 2.5, 1);
+        key('f', 3.5, 1);
+        key('g', 4.5, 1);
+        key('h', 5.5, 1);
+        key('j', 6.5, 1);
+        key('k', 7.5, 1);
+        key('l', 8.5, 1);
+        key('z', 1.5, 2);
+        key('x', 2.5, 2);
+        key('c', 3.5, 2);
+        key('v', 4.5, 2);
+        key('b', 5.5, 2);
+        key('n', 6.5, 2);
+        key('m', 7.5, 2);
+        case ' ':
+            return (KeyOffset) {
+                .width_offset = 2.5,
+                .width_percentage = (KEY_WIDTH_PERCENTAGE * 5),
+                .height_offset = 3,
+                .height_percentage = KEY_HEIGHT_PERCENTAGE
+            };
+
         default:
-            ret = (KeyOffset) {.width_offset=2.5, .width_percentage=(KEY_WIDTH_PERCENTAGE *
-                               5), .height_offset=3, .height_percentage=KEY_HEIGHT_PERCENTAGE};
-            break;
+            return (KeyOffset) {0, 0, 0, 0};
     }
-    return
-            ret;
 }
 
 void computeCurves(Config *config) {
