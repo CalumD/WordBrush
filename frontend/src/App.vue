5a7dd5797de44df058c08b75933eeb39a9fac5d7
CalumD <github@clumd.com>
2020-09-26 01:23:21 +0100

    First (bad) attempt at getting Vue to work as the primary frontend technology for the project
    npm installed a few things, and the webpage builds - but nothing is displayed and I haven't figured out what I need to do to fix yet.
    
    Signed-off-by: CalumD <github@clumd.com>

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app">
+        <img alt="WordBrush Image" src="./assets/test.svg">
+        <WordBrush :msg="message"/>
+    </div>
+</template>
+
+<script>
+import WordBrush from './components/WordBrush.vue'
+
+export default {
+    name: 'app',
+    components: {
+        WordBrush
+    },
+    data() {
+        return {
+            message: 'Welcome to word brush App!'
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
