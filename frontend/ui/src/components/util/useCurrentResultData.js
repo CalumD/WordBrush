869f030b5f4eb5d6c0ddfdae398e02bee3135bd4
CalumD <github@clumd.com>
2021-01-16 09:16:45 +0000

    Link the frontend to the live backend API
    Signed-off-by: CalumD <github@clumd.com>

--- frontend/ui/src/components/util/useCurrentResultData.js
+++ frontend/ui/src/components/util/useCurrentResultData.js
@@ -1,59 +1,68 @@
 import {reactive, readonly} from 'vue';
+import axios from "axios";
+import {BASE_URL} from '@/main';
 
 const currentResultData = reactive({
     resultID: '',
-    images: []
+    outputType: '',
+    images: {}
 });
+const delay = ms => new Promise(res => setTimeout(res, ms));
+const MAX_RETRIES = 5;
+
+function fetchResultSet(resultSetID) {
+    return new Promise((resolve) => {
+        axios
+            .get(
+                `http://${BASE_URL}/api/v1/results/${resultSetID}`,
+                {
+                    responseType: "json",
+                    timeout: 5000
+                }
+            )
+            .then((response) => {
+                if (response.status !== 202) {
+                    currentResultData.resultID = resultSetID;
+                    currentResultData.outputType = response.data.outputType;
+                    currentResultData.images = response.data.words;
+                    return resolve(true);
+                }
+                return resolve(false);
+            })
+            .catch((err) => {
+                console.log('Error retrieving result set: ', err);
+                currentResultData.resultID = '';
+                currentResultData.outputType = '';
+                currentResultData.images = {};
+                return resolve(false);
+            });
+    });
+}
 
 export function useCurrentResultData() {
-
-    if (currentResultData.resultID === '') {
-        search();
-    }
 
     function getCurrentResult() {
         return readonly(currentResultData);
     }
 
-    function search(url, opts) {
-        console.log("Setting 1");
-        currentResultData.resultID = '458ddcfd-7212-470e-9036-bbfb40a35f88';
-        currentResultData.images = [
-            {imageID: '0.svg', imageText: '6785'},
-            {imageID: '1.svg', imageText: '=1abcdef!'},
-            {imageID: '2.svg', imageText: 'The'},
-            {imageID: '3.svg', imageText: 'quick'},
-            {imageID: '4.svg', imageText: 'brown'},
-            {imageID: '5.svg', imageText: 'fox'},
-            {imageID: '6.svg', imageText: 'jumps'},
-            {imageID: '7.svg', imageText: 'over'},
-            {imageID: '8.svg', imageText: 'the'},
-            {imageID: '9.svg', imageText: 'lazy'},
-            {imageID: '10.svg', imageText: 'dog'}
-        ]
-    }
+    async function fetchNewResultSet(resultSetID) {
+        let success = await fetchResultSet(resultSetID);
+        let retries = MAX_RETRIES;
 
-    function search2Tho(url, opts) {
-        console.log("Setting 2");
-        currentResultData.resultID = 'd3aa88e2-c754-41e0-8ba6-4198a34aa0a2';
-        currentResultData.images = [
-            {imageID: '80.svg', imageText: 'AARC'},
-            {imageID: '81.svg', imageText: 'aardvark!'},
-            {imageID: '82.svg', imageText: 'aardvarks'},
-            {imageID: '83.svg', imageText: 'aardwolf'},
-            {imageID: '84.svg', imageText: 'aardwolves'},
-            {imageID: '85.svg', imageText: 'Aaren'},
-            {imageID: '86.svg', imageText: 'Aargau'},
-            {imageID: '87.svg', imageText: 'aargh'},
-            {imageID: '88.svg', imageText: 'Aarhus'},
-            {imageID: '89.svg', imageText: 'Aarika'},
-            {imageID: '90.svg', imageText: 'Aaron'}
-        ]
+        while (!success && retries-- > 0) {
+            await delay(2000);
+            success = await fetchResultSet(resultSetID);
+        }
+        if (!success) {
+            console.error('Couldn\'t get a response in time for a resultSet (' + resultSetID + '), giving up.');
+            currentResultData.resultID = '';
+            currentResultData.outputType = '';
+            currentResultData.images = {};
+        }
     }
 
     return {
         getCurrentResult,
-        search,
-        search2Tho
+        fetchNewResultSet
     };
 }
